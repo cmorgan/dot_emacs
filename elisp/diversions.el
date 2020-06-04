4c6ab75b4a2b4631d22502221379b80d13ea0aae
Chris Morgan <christopher.morgan@db.com>
2020-06-04 08:55:33 +0100

    update

---
+++ elisp/diversions.el
@@ -0,0 +1,14 @@
+
+; https://nullprogram.com/blog/2012/09/14/
+(defun sierpinski (s)
+  (pop-to-buffer (get-buffer-create "*sierpinski*"))
+  (fundamental-mode) (erase-buffer)
+  (cl-labels ((fill-p (x y)
+                   (cond ((or (zerop x) (zerop y)) "0")
+                         ((and (= 1 (mod x 3)) (= 1 (mod y 3))) "1")
+                         (t (fill-p (/ x 3) (/ y 3))))))
+    (insert (format "P1\n%d %d\n" s s))
+    (dotimes (y s) (dotimes (x s) (insert (fill-p x y) " "))))
+  (image-mode))
+
+(sierpinski (expt 3 5))
