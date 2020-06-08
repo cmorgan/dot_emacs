f7a383f8c0fa6a68f0e425eef51edde56d6e394b
Chris Morgan <christopher.morgan@db.com>
2020-06-08 16:11:16 +0100

    add pytest and py runner scripts

--- init.el
+++ init.el
@@ -21,6 +21,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
+ '(package-selected-packages
+   (quote
+    (org-fragtog zetteldeft yasnippet yaml-mode writeroom-mode which-key use-package spacemacs-theme spaceline smartparens restart-emacs ranger python-black persp-projectile org-roam org-cliplink markdown-mode key-chord jinja2-mode hl-todo helpful general flycheck exec-path-from-shell evil-visualstar evil-surround evil-org evil-nerd-commenter evil-escape evil-collection eshell-bookmark elisp-format dumb-jump dired-narrow cython-mode csharp-mode counsel-projectile company-restclient auto-compile anaconda-mode ag ace-window)))
  '(safe-local-variable-values (quote ((eval org-latex-preview)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
