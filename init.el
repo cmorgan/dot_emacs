57db5bf584f0f3dc042b8d31f9d2a4a46498d8d8
Chris <chris.j.morgan@gmail.com>
2020-06-08 08:56:19 +0100

    zetteldeft

--- init.el
+++ init.el
@@ -16,25 +16,15 @@
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
- '(package-selected-packages
-   (quote
-    (ace-window avy zetteldeft deft elisp-format cython-mode org-roam jinja2-mode org-cliplink elisp-refs dumb-jump diff-hl dash-functional dash counsel company csharp-mode flycheck-aspell pomodoro evil-visualstar persp-mode mmm-mode rst-mode markdown-mode smartparens smartparens-config gruvbox-theme yaml-mode restart-emcas magit yasnippet restart-emacs company-jedi eshell-bookmark hl-todo fic-mode python-black aweshell highlight-indent-guides company-anaconda anaconda-mode anaconda spacemacs-theme dired-narrow persp-projectile perspective evil-escape counsel-projectile moody solarized-theme undo-tree evil-org evil-surround evil-collection evil use-package goto-chg auto-compile))))
+ '(safe-local-variable-values (quote ((eval org-latex-preview)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
-
