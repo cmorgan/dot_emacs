3ebd9cdedf25c84818185a352ac2a04a6a9b7f08
Chris <chris.j.morgan@gmail.com>
2020-06-20 11:04:36 +0100

    init.el

--- init.el
+++ init.el
@@ -13,27 +13,12 @@
 (when (not (package-installed-p 'use-package)) (package-refresh-contents)
   (package-install 'use-package))
 
+
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
+;; change the default path so we don't store these in init.el, they won't be picked up if not loaded
+(setq custom-file (concat user-emacs-directory "custom.el"))
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
- '(deft-directory "c:/Users/Public/dev/docs/notes")
- '(deft-extensions (quote ("org" "md" "txt")))
- '(deft-use-filename-as-title t)
- '(org-agenda-files
-   (quote
-    ("c:/dev/docs/org/index.org" "c:/dev/docs/org/research.org" "c:/dev/docs/org/todo.org")))
- '(package-selected-packages
-   (quote
-    (cpu-stats mode-line-stats org-download evil-lispy lispy org-plus-contrib org-fragtog zetteldeft yasnippet yaml-mode writeroom-mode which-key use-package spacemacs-theme spaceline smartparens restart-emacs ranger python-black persp-projectile org-roam org-cliplink markdown-mode key-chord jinja2-mode hl-todo helpful general flycheck exec-path-from-shell evil-visualstar evil-surround evil-org evil-nerd-commenter evil-escape evil-collection eshell-bookmark elisp-format dumb-jump dired-narrow cython-mode csharp-mode counsel-projectile company-restclient auto-compile anaconda-mode ag ace-window)))
- '(safe-local-variable-values (quote ((eval org-latex-preview)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
