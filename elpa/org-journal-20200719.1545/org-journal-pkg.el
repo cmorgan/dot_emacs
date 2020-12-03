12dc856cf8a65f0622675d40c5658217c7113b40
Chris <chris.j.morgan@gmail.com>
2020-12-03 18:04:44 +0000

    add elisp

---
+++ elpa/org-journal-20200719.1545/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-journal" "20200719.1545" "a simple org-mode based journaling mode" '((emacs "25.1") (org "9.1")) :commit "a6378dc484ed8df7d2d2f05ac70bd9196aa6f4c0" :authors '(("Bastian Bechtold") ("Christian Schwarzgruber")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
